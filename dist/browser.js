dbb42adf7eadddfc0663600f5119057c122cce38
yangshengtao <shengtao.yst@alibaba-inc.com>
2016-09-09 11:33:27 +0800

    * add node file

--- dist/browser.js
+++ dist/browser.js
@@ -1,4 +1,4 @@
-(this.nativeLog || function(s) {console.log(s)})('START WEEX HTML5: 0.3.2 Build 20160831');
+(this.nativeLog || function(s) {console.log(s)})('START WEEX HTML5: 0.3.2 Build 20160909');
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
