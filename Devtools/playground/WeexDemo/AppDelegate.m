249f84012793aa6c4db4a08ef2921af40e5cb91b
yangshengtao <shengtao.yst@alibaba-inc.com>
2016-12-23 15:03:22 +0800

    update package.json

--- Devtools/playground/WeexDemo/AppDelegate.m
+++ Devtools/playground/WeexDemo/AppDelegate.m
@@ -29,7 +29,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-    
+
     [self initWeexSDK];
     
     self.window.rootViewController = [[WXRootViewController alloc] initWithRootViewController:[self demoController]];
@@ -108,7 +108,7 @@
     
 #if DEBUG
     //If you are debugging in device , please change the host to current IP of your computer.
-    ((WXDemoViewController *)demo).url = [NSURL URLWithString:BUNDLE_URL];
+    ((WXDemoViewController *)demo).url = [NSURL URLWithString:HOME_URL];
 #else
     ((WXDemoViewController *)demo).url = [NSURL URLWithString:BUNDLE_URL];
 #endif
