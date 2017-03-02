bca9559dfc30198f839c4d9d31ffcf1ec1ded4f7
yangshengtao <yangshengtao1314@163.com>
2017-03-02 11:37:21 +0800

    update umbrella header

--- Devtools/WXDevtool/WXDevTool/TBWXDevTool.h
+++ Devtools/WXDevtool/WXDevTool/TBWXDevTool.h
@@ -16,6 +16,6 @@ FOUNDATION_EXPORT const unsigned char WXDevToolVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WXDevTool/PublicHeader.h>
 
-#import <WXDevTool/WXDevTool.h>
+#import <TBWXDevTool/WXDevTool.h>
 
 
