ec6399bd7ba686d35de3cb6ad9f07fedf679483b
doumafang <doumafang@gmail.com>
2018-08-08 12:16:51 +0800

    [sdk] support recycle list debug

--- sdk/WXDevTool/Source/PonyDebugger/WXDebugger.m
+++ sdk/WXDevTool/Source/PonyDebugger/WXDebugger.m
@@ -77,6 +77,7 @@ void _WXLogObjectsImpl(NSString *severity, NSArray *arguments)
     NSThread    *_inspectThread;
     NSString    *_registerData;
     NSString * _instanceID;
+    NSURL *_syncHttpUrl;
 }
 
 + (WXDebugger *)defaultInstance;
@@ -133,17 +134,14 @@ void _WXLogObjectsImpl(NSString *severity, NSArray *arguments)
 - (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(NSString *)message;
 {
     NSDictionary *obj = [NSJSONSerialization JSONObjectWithData:[message dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
-
     NSString *fullMethodName = [obj objectForKey:@"method"];
     NSInteger dotPosition = [fullMethodName rangeOfString:@"."].location;
     NSString *domainName = [fullMethodName substringToIndex:dotPosition];
     NSString *methodName = [fullMethodName substringFromIndex:dotPosition + 1];
     NSString *objectID = [obj objectForKey:@"id"];
-
     WXResponseCallback responseCallback = ^(NSDictionary *result, id error) {
         NSMutableDictionary *response = [[NSMutableDictionary alloc] initWithCapacity:2];
         [response setValue:objectID forKey:@"id"];
-
         if (result) {
             NSMutableDictionary *newResult = [[NSMutableDictionary alloc] initWithCapacity:result.count];
             [result enumerateKeysAndObjectsUsingBlock:^(id key, id val, BOOL *stop) {
@@ -154,7 +152,6 @@ void _WXLogObjectsImpl(NSString *severity, NSArray *arguments)
             NSMutableDictionary *newResult = [[NSMutableDictionary alloc] init];
             [response setObject:newResult forKey:@"result"];
         }
-
         NSData *data = [NSJSONSerialization dataWithJSONObject:response options:0 error:nil];
         NSString *encodedData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         [webSocket send:encodedData];
@@ -344,6 +341,11 @@ void _WXLogObjectsImpl(NSString *severity, NSArray *arguments)
     _socket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:url]];
     _socket.delegate = self;
     [_socket open];
+    
+    NSString *urlStr = url.absoluteString;
+    NSString *httpStr = [urlStr stringByReplacingOccurrencesOfString:@"ws" withString:@"http"];
+    NSString *finalUrlStr = [httpStr stringByReplacingOccurrencesOfString:@"debugProxy/native" withString:@"syncCallJS"];
+    _syncHttpUrl = [NSURL URLWithString:finalUrlStr];
 }
 
 - (BOOL)isConnected;
@@ -620,8 +622,24 @@ void _WXLogObjectsImpl(NSString *severity, NSArray *arguments)
     [dict setObject:@"WxDebug.callJS" forKey:@"method"];
     [dict setObject:params forKey:@"params"];
     
-    [_debugAry addObject:[WXUtility JSONString:dict]];
-    [self _executionDebugAry];
+    NSString *argsStr = [args componentsJoinedByString:@""];
+    if ([argsStr containsString:@"componentHook"]) {
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:_syncHttpUrl];
+        NSData *data =  [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
+        [request setHTTPMethod:@"POST"];
+        [request setHTTPBody:data];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        NSURLResponse *response = nil;
+        NSError *error = nil;
+        NSData *receivedData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        NSDictionary *receiveDic = (NSDictionary*) [NSKeyedUnarchiver unarchiveObjectWithData:receivedData];
+        return (JSValue*) receiveDic;
+    }
+    else
+    {
+        [_debugAry addObject:[WXUtility JSONString:dict]];
+        [self _executionDebugAry];
+    }
     return nil;
 }
 
